46f0858823458dfb0451da606d4f27e24d96012b
hurdoo <64835865+HURDOO@users.noreply.github.com>
2022-09-04 20:17:50 +0900

    Update problem loading to use url from settings.yml

--- problem/views.py
+++ problem/views.py
@@ -6,7 +6,7 @@ from . import load
 def index(request, problem):
     data = {
         'problem_id': problem,
-        'problem_url': f'https://raw.githubusercontent.com/HURDOO/python-trainer/master/.problems/{problem}.md'
+        'problem_url': load.PROBLEM_URL.format(problem, problem)
     }
     data.update(info.get_data(request.session))
 
