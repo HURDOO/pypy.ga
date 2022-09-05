f561091469a50014d6d635ba3d5299ed2ba0623c
hurdoo <64835865+HURDOO@users.noreply.github.com>
2022-09-05 23:19:59 +0900

    Renew submit detail

--- submit/views.py
+++ submit/views.py
@@ -1,6 +1,10 @@
+import json
+
 from django.shortcuts import render, redirect
-from .models import Submit, SubmitType, getSubmitType
+from .models import Submit, ResultType, SubmitType, getSubmitType
 from runner import runner
+from account import info
+from urllib.parse import quote
 
 
 def new(request):
@@ -36,21 +40,50 @@ def new(request):
 
 def detail(request, submit_id):
     submit = Submit.objects.get(id=submit_id)
-    return render(request, 'detail.html', context={
+    data = {
         'submit_id': submit.id,
         'problem_id': submit.problem_id,
         'user_id': submit.user_id,
         'submit_type': submit.type,
         'result': submit.result,
-        'memory_usage': submit.memory_usage,
-        'time_usage': submit.time_usage,
-        'submit_time': submit.submit_time,
         'code': submit.code,
         'code_length': submit.code_length,
-        'stdin': submit.stdin,
-        'stdout': submit.stdout,
-        'stderr': submit.stderr
-    })
+        'submit_time': str(submit.submit_time)
+    }
+    data.update(get_details(submit))
+    data.update(info.get_data(request.session))
+    return render(request, 'detail.html', context=data)
+
+
+def get_details(submit: Submit) -> dict:
+    if submit.type == SubmitType.GRADE:
+        if submit.result == ResultType.ACCEPTED:
+            return {
+                'time_usage': submit.time_usage,
+                'memory_usage': submit.memory_usage,
+            }
+
+    else:
+        if submit.result == ResultType.COMPLETE:
+            return {
+                'stdin': submit.stdin,
+                'stdout': submit.stdout,
+                'time_usage': submit.time_usage,
+                'memory_usage': submit.memory_usage,
+            }
+        elif submit.result in [ResultType.TIME_LIMIT, ResultType.MEMORY_LIMIT]:
+            return {
+                'stdin': submit.stdin,
+                'stdout': submit.stdout,
+            }
+        elif submit.result == ResultType.RUNTIME_ERROR:
+            return {
+                'stdin': submit.stdin,
+                'stdout': submit.stdout,
+                'stderr': submit.stderr
+            }
+
+    return {}
 
 
 def index(request):
