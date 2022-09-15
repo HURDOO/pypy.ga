d53237ebcc6747009e2465691db428c9859ebfb2
hurdoo <64835865+HURDOO@users.noreply.github.com>
2022-09-16 01:29:33 +0900

    Add 10008

---
+++ .problems/10008/gen.py
@@ -0,0 +1,36 @@
+import os
+import random
+from pathlib import Path
+
+
+def get_input_output() -> list[tuple[str, str]]:
+    lst = [('2', '사람'), ('3', '원숭이')]
+    random.seed(10008)
+    for _ in range(5):
+        n = random.randint(0, 1000000)
+        lst.append((str(n), '원숭이'))
+
+    lst.extend([('귀요미', '원숭이'), ('몰라', '원숭이'), ('우끼끼', '원숭이'), ('우끼끾끼!!', '원숭이'), ('🐵 (해맑)', '원숭이')])
+    return lst
+
+
+PROBLEM_DIR = Path(__file__).resolve().parent
+INPUT_DIR = PROBLEM_DIR / 'in'
+OUTPUT_DIR = PROBLEM_DIR / 'out'
+INPUT_FILENAME = '{}.in'
+OUTPUT_FILENAME = '{}.out'
+
+if not os.path.exists(INPUT_DIR):
+    os.mkdir(INPUT_DIR)
+
+if not os.path.exists(OUTPUT_DIR):
+    os.mkdir(OUTPUT_DIR)
+
+case_list = get_input_output()
+
+for idx in range(len(case_list)):
+    stdin, stdout = case_list[idx]
+    with open(INPUT_DIR / INPUT_FILENAME.format(idx + 1), 'w', encoding='UTF-8') as input_file:
+        input_file.write(stdin + '\n')
+    with open(OUTPUT_DIR / OUTPUT_FILENAME.format(idx + 1), 'w', encoding='UTF-8') as output_file:
+        output_file.write(stdout + '\n')
