9f08e9b46839ef9a05a449b4fb925e4ed93da51e
hurdoo <64835865+HURDOO@users.noreply.github.com>
2022-09-16 08:55:39 +0900

    Update 10010

--- .problems/10010/gen.py
+++ .problems/10010/gen.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 
 def get_input_output() -> list[tuple[str, str]]:
-    lst = [('1\n1', '평행'), ('1\n-1', '수직'), ('1\n0', '만남')]
+    lst = [('1\n1', '평행'), ('1\n0', '만남'), ('1\n-1', '만남\n수직')]
 
     random.seed(10010)
 
@@ -15,17 +15,17 @@ def get_input_output() -> list[tuple[str, str]]:
         lst.append((f'{n}\n{n}', '평행'))
 
     for _ in range(30):
-        n = 0
-        while n == 0:
-            n = random.randint(-uk, uk)
-        lst.append((f'{n}\n{-n}', '수직'))
-
-    for _ in range(30):
         a = b = 0
         while a == b:
             a = random.randint(-uk, uk)
             b = random.randint(-uk, uk)
         lst.append((f'{a}\n{b}', '만남'))
+
+    for _ in range(30):
+        n = 0
+        while n == 0:
+            n = random.randint(-uk, uk)
+        lst.append((f'{n}\n{-n}', '만남\n수직'))
 
     return lst
 
