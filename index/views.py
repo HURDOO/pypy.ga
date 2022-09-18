481246fe3e3e42fc043181406bde56ab82f1f543
hurdoo <64835865+HURDOO@users.noreply.github.com>
2022-09-18 20:51:27 +0900

    Fix loading last user score when logged out

--- index/views.py
+++ index/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 from account import info
 from problem import load
 from account.models import Account
+from copy import deepcopy
 
 
 def index(request):
     if len(load.PROBLEMS_LIST) == 0:
         load.load_problems()
-    categories = load.PROBLEMS_LIST
+    categories = deepcopy(load.PROBLEMS_LIST)
+
     user_id = info.get_user_id(request.session)
     if user_id is not None:
         submits = Account.objects.get(id=user_id).submits
@@ -15,6 +17,7 @@ def index(request):
             for problem in category['problems']:
                 if str(problem['id']) in submits:
                     problem['score'] = submits[str(problem['id'])]['score']
+
     data = {'list': categories}
     data.update(info.get_data(request.session))
     return render(request, 'index.html', data)
