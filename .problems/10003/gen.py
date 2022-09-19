e0d5a51dbde2ce521f2bd973ecc979748b27fd1e
hurdoo <64835865+HURDOO@users.noreply.github.com>
2022-09-19 21:38:46 +0900

    Fix all cases to use LF

--- .problems/10003/gen.py
+++ .problems/10003/gen.py
@@ -1,32 +1,6 @@
-import os
-from pathlib import Path
-
-
 def get_input_output() -> list[tuple[str, str]]:
     lst = []
     for s in '유은미, 황광희, 정희선, 봉인태, 예미영, 설현빈, 문남혁, 이현빈, 황은경, 강연진' \
             ', 문민웅, 홍영우, 권선희, 제갈문철, 류명진, 문시우, 봉설현, 김다연, 복용남, 남궁지희'.split(', '):
         lst.append((s, s))
     return lst
-
-
-PROBLEM_DIR = Path(__file__).resolve().parent
-INPUT_DIR = PROBLEM_DIR / 'in'
-OUTPUT_DIR = PROBLEM_DIR / 'out'
-INPUT_FILENAME = '{}.in'
-OUTPUT_FILENAME = '{}.out'
-
-if not os.path.exists(INPUT_DIR):
-    os.mkdir(INPUT_DIR)
-
-if not os.path.exists(OUTPUT_DIR):
-    os.mkdir(OUTPUT_DIR)
-
-case_list = get_input_output()
-
-for idx in range(len(case_list)):
-    stdin, stdout = case_list[idx]
-    with open(INPUT_DIR / INPUT_FILENAME.format(idx+1), 'w', encoding='UTF-8') as input_file:
-        input_file.write(stdin + '\n')
-    with open(OUTPUT_DIR / OUTPUT_FILENAME.format(idx+1), 'w', encoding='UTF-8') as output_file:
-        output_file.write(stdout + '\n')
--- .problems/10011/gen.py
+++ .problems/10011/gen.py
@@ -1,6 +1,4 @@
-import os
 import random
-from pathlib import Path
 
 
 def get_input_output() -> list[tuple[str, str]]:
@@ -14,19 +12,19 @@ def get_input_output() -> list[tuple[str, str]]:
         a = random.randint(1, 100) * 100
         b = random.randint(1, 100) * 100
         c = random.randint(1, 100) * 100
-        hap = a+b+c
+        hap = a + b + c
 
         d = hap - random.randint(0, 100) * 100
-        e = d + random.randint(0, (hap-d)//100) * 100
-        lst.append((f'{a}\n{b}\n{c}\n{d}\n{e}', f'{hap}원'))
+        e = d + random.randint(0, (hap - d) // 100) * 100
+        lst.append((get_input(a, b, c, d, e), f'{hap}원'))
 
         d = hap - random.randint(0, 100) * 100
         e = hap + random.randint(0, 100) * 100
-        lst.append((f'{a}\n{b}\n{c}\n{d}\n{e}', f'{hap+4000}원'))
+        lst.append((get_input(a, b, c, d, e), f'{hap + 4000}원'))
 
         d = hap + random.randint(0, 100) * 100
         e = d + random.randint(0, 100) * 100
-        lst.append((f'{a}\n{b}\n{c}\n{d}\n{e}', '배달 불가'))
+        lst.append((get_input(a, b, c, d, e), '배달 불가'))
 
     uk = 100000000
 
@@ -34,51 +32,33 @@ def get_input_output() -> list[tuple[str, str]]:
         a = random.randint(1, uk)
         b = random.randint(1, uk)
         c = random.randint(1, uk)
-        hap = a+b+c
+        hap = a + b + c
 
-        d = random.randint(0, uk*3)
-        e = d + random.randint(0, abs(hap-d))
-        lst.append((f'{a}\n{b}\n{c}\n{d}\n{e}',
-                   (lambda: '배달 불가' if hap < d
-                    else (lambda: f'{hap+4000}원' if hap < e else f'{hap}원')()
-                    )()
+        d = random.randint(0, uk * 3)
+        e = d + random.randint(0, abs(hap - d))
+        lst.append((get_input(a, b, c, d, e),
+                    (lambda: '배달 불가' if hap < d
+                    else (lambda: f'{hap + 4000}원' if hap < e else f'{hap}원')()
+                     )()
                     ))
 
     for _ in range(6):
         a = random.randint(1, uk)
         b = random.randint(1, uk)
         c = random.randint(1, uk)
-        hap = a+b+c
+        hap = a + b + c
 
         d, e = 0, 0
-        lst.append((f'{a}\n{b}\n{c}\n{d}\n{e}', f'{hap}원'))
+        lst.append((get_input(a, b, c, d, e), f'{hap}원'))
 
-        d, e = 0, hap + random.randint(0, uk*3-hap)
-        lst.append((f'{a}\n{b}\n{c}\n{d}\n{e}', f'{hap+4000}원'))
+        d, e = 0, hap + random.randint(0, uk * 3 - hap)
+        lst.append((get_input(a, b, c, d, e), f'{hap + 4000}원'))
 
-        d, e = hap - random.randint(0, uk*3-hap), 0
-        lst.append((f'{a}\n{b}\n{c}\n{d}\n{e}', f'{hap}원'))
+        d, e = hap - random.randint(0, uk * 3 - hap), 0
+        lst.append((get_input(a, b, c, d, e), f'{hap}원'))
 
     return lst
 
 
-PROBLEM_DIR = Path(__file__).resolve().parent
-INPUT_DIR = PROBLEM_DIR / 'in'
-OUTPUT_DIR = PROBLEM_DIR / 'out'
-INPUT_FILENAME = '{}.in'
-OUTPUT_FILENAME = '{}.out'
-
-if not os.path.exists(INPUT_DIR):
-    os.mkdir(INPUT_DIR)
-
-if not os.path.exists(OUTPUT_DIR):
-    os.mkdir(OUTPUT_DIR)
-
-case_list = get_input_output()
-
-for idx in range(len(case_list)):
-    stdin, stdout = case_list[idx]
-    with open(INPUT_DIR / INPUT_FILENAME.format(idx + 1), 'w', encoding='UTF-8') as input_file:
-        input_file.write(stdin + '\n')
-    with open(OUTPUT_DIR / OUTPUT_FILENAME.format(idx + 1), 'w', encoding='UTF-8') as output_file:
-        output_file.write(stdout + '\n')
+def get_input(a: int, b: int, c: int, d: int, e: int) -> str:
+    return f'{a}\n{b}\n{c}\n{d}\n{e}'
