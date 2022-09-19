13a272ceb0fa86769e31e43725dfddd391eb2230
hurdoo <64835865+HURDOO@users.noreply.github.com>
2022-09-19 16:34:41 +0900

    Update 10013

--- .problems/10013/gen.py
+++ .problems/10013/gen.py
@@ -6,12 +6,11 @@ from pathlib import Path
 def get_input_output() -> list[tuple[str, str]]:
     lst = []
 
-    for a, aa in [('1', '짜장면'), ('2', '짬뽕')]:
-        for b, bb in [('3', '신라면'), ('4', '진라면')]:
-            for c, cc in [('5', '물냉면'), ('6', '비빔냉면')]:
-                for d, dd in [('7', '파스타'), ('8', '스파게티')]:
-                    lst.append((f'{a}\n{b}\n{c}\n{d}',
-                                f'{aa}하고 {bb}하고 {cc}하고 {dd}'))
+    for a in ['짜장면', '짬뽕']:
+        for b in ['신라면', '진라면']:
+            for c in ['물냉면', '비빔냉면']:
+                for d in ['파스타', '스파게티']:
+                    lst.append((f'{a}\n{b}\n{c}\n{d}', f'{a}하고 {b}하고 {c}하고 {d}'))
 
     return lst
 
