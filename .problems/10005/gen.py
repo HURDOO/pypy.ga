9e59be0da136591fe3c005bc8bec0f6be574e30e
hurdoo <64835865+HURDOO@users.noreply.github.com>
2022-09-19 21:43:53 +0900

    Update gen.py

--- .problems/10005/gen.py
+++ .problems/10005/gen.py
@@ -1,7 +1,3 @@
-import os
-from pathlib import Path
-
-
 def get_input_output() -> list[tuple[str, str]]:
     lst = []
     for s in '우영우, 동그라미, 사공용욱, 남석희, 송선호, 황종철, 정우정, 예우철, 김정우, 홍경철' \
@@ -17,25 +13,3 @@ def get_input_output() -> list[tuple[str, str]]:
             .split(', '):
         lst.append((s, s[0] + ' to the ' + s[1] + ' to the ' + s[2:]))
     return lst
-
-
-PROBLEM_DIR = Path(__file__).resolve().parent
-INPUT_DIR = PROBLEM_DIR / 'in'
-OUTPUT_DIR = PROBLEM_DIR / 'out'
-INPUT_FILENAME = '{}.in'
-OUTPUT_FILENAME = '{}.out'
-
-if not os.path.exists(INPUT_DIR):
-    os.mkdir(INPUT_DIR)
-
-if not os.path.exists(OUTPUT_DIR):
-    os.mkdir(OUTPUT_DIR)
-
-case_list = get_input_output()
-
-for idx in range(len(case_list)):
-    stdin, stdout = case_list[idx]
-    with open(INPUT_DIR / INPUT_FILENAME.format(idx + 1), 'w', encoding='UTF-8') as input_file:
-        input_file.write(stdin + '\n')
-    with open(OUTPUT_DIR / OUTPUT_FILENAME.format(idx + 1), 'w', encoding='UTF-8') as output_file:
-        output_file.write(stdout + '\n')
--- .problems/10013/gen.py
+++ .problems/10013/gen.py
@@ -1,8 +1,3 @@
-import os
-import random
-from pathlib import Path
-
-
 def get_input_output() -> list[tuple[str, str]]:
     lst = []
 
@@ -13,25 +8,3 @@ def get_input_output() -> list[tuple[str, str]]:
                     lst.append((f'{a}\n{b}\n{c}\n{d}', f'{a}하고 {b}하고 {c}하고 {d}'))
 
     return lst
-
-
-PROBLEM_DIR = Path(__file__).resolve().parent
-INPUT_DIR = PROBLEM_DIR / 'in'
-OUTPUT_DIR = PROBLEM_DIR / 'out'
-INPUT_FILENAME = '{}.in'
-OUTPUT_FILENAME = '{}.out'
-
-if not os.path.exists(INPUT_DIR):
-    os.mkdir(INPUT_DIR)
-
-if not os.path.exists(OUTPUT_DIR):
-    os.mkdir(OUTPUT_DIR)
-
-case_list = get_input_output()
-
-for idx in range(len(case_list)):
-    stdin, stdout = case_list[idx]
-    with open(INPUT_DIR / INPUT_FILENAME.format(idx + 1), 'w', encoding='UTF-8') as input_file:
-        input_file.write(stdin + '\n')
-    with open(OUTPUT_DIR / OUTPUT_FILENAME.format(idx + 1), 'w', encoding='UTF-8') as output_file:
-        output_file.write(stdout + '\n')
